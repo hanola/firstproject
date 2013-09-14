e02f845e3a6ad1c1cdc6d3531606919dc26b57d7
Olav Brenna Hansen <post@vasetskiheiser.no>
2013-09-14 13:43:53 +0200

    Save state working although not in landscape view.

--- FirstProject/src/no/example/firstproject/MainActivity.java
+++ FirstProject/src/no/example/firstproject/MainActivity.java
@@ -2,19 +2,94 @@ package no.example.firstproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-	
+	private static final String TAG = "no.example.firstproject";
+	private EditText editBox;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate");
+        editBox =(EditText)findViewById(R.id.saved);
     }
-       //Actions when pressing a button
+	
+	protected void onStart() {
+		super.onStart();
+		Log.i(TAG, "onStart");
+	}
+	
+	protected void onResume() {
+		super.onResume();
+		Log.i(TAG, "onResume");
+		SharedPreferences prefs = getPreferences(0); 
+        String restoredText = prefs.getString("text", null);
+        if (restoredText != null) {
+            editBox.setText(restoredText, TextView.BufferType.EDITABLE);
+ 
+            int selectionStart = prefs.getInt("selection-start", -1);
+            int selectionEnd = prefs.getInt("selection-end", -1);
+            if (selectionStart != -1 && selectionEnd != -1) {
+                editBox.setSelection(selectionStart, selectionEnd);
+            }
+        }
+	}
+	
+	protected void onPause() {
+		super.onPause();
+		Log.i(TAG, "onPause");
+		 SharedPreferences.Editor editor = getPreferences(0).edit();
+	        editor.putString("text", editBox.getText().toString());
+	        editor.putInt("selection-start", editBox.getSelectionStart());
+	        editor.putInt("selection-end", editBox.getSelectionEnd());
+	        editor.commit();
 		
+		
+	}
+	
+	protected void onStop() {
+		super.onStop();
+		Log.i(TAG, "onStop");
+	}
+	
+	protected void onRestart() {
+		super.onRestart();
+		Log.i(TAG, "onRestart");
+	}
+	
+	protected void onDestory() {
+		super.onDestroy();
+		Log.i(TAG, "onDestroy");
+	}
+	
+	protected void onSaveInstanceState(Bundle outState){
+		super.onSaveInstanceState(outState);
+		Log.i(TAG, "onSaveInstanceState");
+		
+		//final EditText textBox = (EditText) findViewById(R.id.editText4);
+		//CharSequence userText = textBox.getText();
+		//outState.putCharSequence("savedText", userText);
+	}
+	
+	protected void onRestoreInstanceState(Bundle savedState){
+		//super.onRestoreInstanceState(savedState);
+		Log.i(TAG, "onRestoreInstanceState");
+		
+		//final EditText textBox = (EditText) findViewById(R.id.editText4);
+		//CharSequence userText = savedState.getCharSequence("savedText");
+		
+		//textBox.setText(userText);
+		
+	}
+	
+       //Actions when pressing a button
 	//Go to download page
 	public void goToDownloadDemo(View view) {
 	    Intent intent = new Intent(this, DownloadMainActivity.class);
@@ -30,4 +105,5 @@ public class MainActivity extends Activity {
 	    Intent intent = new Intent(this, GooglemapActivity.class);
 	    startActivity(intent);
 	    }
+	
 }
