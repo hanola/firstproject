2050645fa670bb4ea6b6fae7e6aa186b1a652c22
Olav Brenna Hansen <post@vasetskiheiser.no>
2013-09-21 17:47:04 +0200

    Changed launch icon

---
+++ FirstProject/src/no/example/firstproject/MainActivityTest.java
@@ -0,0 +1,15 @@
+package no.example.firstproject;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+	public void testOnCreateBundle() {
+		fail("Not yet implemented");
+	}
+
+	public void testGoToSqlDemo() {
+		fail("Not yet implemented");
+	}
+
+}
