35c51805696a79a9d9f424dd65d63e189eecf809
Olav Brenna Hansen <post@vasetskiheiser.no>
2013-10-13 23:27:26 +0200

    After 2

---
+++ FirstProject/src/no/example/firstproject/DownloadMainActivityTest.java
@@ -0,0 +1,19 @@
+package no.example.firstproject;
+
+import junit.framework.TestCase;
+
+public class DownloadMainActivityTest extends TestCase {
+
+	public DownloadMainActivityTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
